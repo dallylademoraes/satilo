b308f2aad1a4c5b6d66f8393852ec40bede766e3
Dallyla de Moraes Sousa <133059854+dallylademoraes@users.noreply.github.com>
2026-01-15 19:50:55 -0300

    main

--- satilo/settings.py
+++ satilo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^$h(qi7$ik^i470ssfw^**x-k4o&ltniq)izz^dxi*9ze8bft+'
+SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.vercel.app,localhost').split(',')
 
 
 # Application definition
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pessoas', 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'auth.User'
 # Media Files (Arquivos enviados pelo usuário, como fotos)
